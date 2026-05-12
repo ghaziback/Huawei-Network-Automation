cbbdebfe958a24aaf95ef39b147a6ac45f5ce1b9
ghaziback <backghazi@gmail.com>
2026-05-12 16:59:26 +0200

    skiped the emty lines and headers and statred to parse into interfaces info

--- Stage2/02_read_info.py
+++ Stage2/02_read_info.py
@@ -3,23 +3,46 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from device_config import DEVICE  # now it works from anywhere
 from netmiko import ConnectHandler
+from device_config import DEVICE 
 
-
+# ─── 1. Connect ───────────────────────────────────────────
 print("Connecting to Router ... ")
 connection = ConnectHandler(**DEVICE)
 print("Connected !\n")
 
-# --- Pull raw output ---
-vlan_output = connection.send_command('dis vla summ')
+
+# ─── 2. Pull raw output ───────────────────────────────────
 info_output = connection.send_command('dis ip interf brie')
-
-print("=== VLAN SUMMARY ===")
-print(vlan_output)
-
-print("\n === IP INTERFACES === ")
-print(info_output)
-
 connection.disconnect()
-print("\n Disconnect Done !!!")
+
+# ─── 3. Parse it ──────────────────────────────────────────
+lines = info_output.splitlines()
+
+# skip junk, keep data
+skip_words = [
+    '*down:', '!down:', '^down:',
+    '(l):', '(s):', '(d):', '(E):',
+    'The number', 'Interface',
+    '---- More ----'
+]
+
+for i ,line in enumerate(lines):
+    #skip emty lines
+    if not line.strip():
+        continue
+
+    #skip header/legened lines
+    if any(word in line for word in skip_words):
+        continue
+
+    # Split the line into parts
+    parts = line.split()
+
+    print(f"parts = {parts}")
+    print(f"  [0] interface = {parts[0]}")
+    print(f"  [1] ip        = {parts[1]}")
+    print(f"  [2] physical  = {parts[2]}")
+    print(f"  [3] protocol  = {parts[3]}")
+    print(f"  [4] vpn       = {parts[4] if len(parts) >= 5 else '--'}")
+    print()
